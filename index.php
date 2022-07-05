a0c0b8085fa35097443690a53100519b0fdbbaed
Gaëllan <mari.doucet@kornog.bzh>
2022-07-05 08:35:31 +0000

    exercice 1

--- index.php
+++ index.php
@@ -9,4 +9,13 @@ $jane = new Customer("Jane", "Doe", 23);
 
 $counter = new Counter(true);
 
+// $jane et $john vont chacun créer une commande (Order) pour le produit "Pizza+Bière".
+
+$orderJohn = new Order($john, "Pizza+Bière");
+$orderJohn->print();
+
+$orderJane = new Order($jane, "Pizza+Bière");
+$orderJane->print();
+
+// Initialisez les deux Order dans le fichier index.php et afficher les avec leur méthode print().
 
